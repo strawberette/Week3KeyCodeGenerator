35b6f64e093dec8064d1fc19d4f71e6fbd9c0b9a
Noemi Scotellaro <noemi.scotellaro@gmail.com>
2021-11-29 22:49:54 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const intro = document.getElementById("intro");
+const container = document.getElementById("container");
+const keyCode = document.getElementById("keyCode");
+const eventKey = document.getElementById("eventKey");
+const eventCode = document.getElementById("eventCode");
+const eventWhich = document.getElementById("eventWhich");
+const keyNumber = document.getElementById("keyNumber");
+const keyEvent = document.getElementById("keyEvent");
+const codeEvent = document.getElementById("codeEvent");
+const whichEvent = document.getElementById("whichEvent");
+container.style.display = "none";
+document.addEventListener("keypress", (input) => {
+    intro.style.display = "none";
+    keyCode.style.display = "flex";
+    container.style.display = "flex";
+    keyNumber.textContent = input.keyCode;
+    keyEvent.textContent = input.key;
+    codeEvent.textContent = input.code;
+    whichEvent.textContent = input.which;
+} );
